0e80cee0c3a4d5bf0e957f6ce175a3cddf7eebe5
Ali Hafez <alihafez42@gmail.com>
2022-05-02 01:47:00 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,176 @@
+// const { Vector3 } = require("./three");
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, (window.innerWidth / window.innerHeight), 0.1, 1000);
+camera.position.y = 2;
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.domElement.style.margin = "0";
+renderer.domElement.style.zIndex = "-1";
+// renderer.domElement.style.position = "fixed";
+
+document.body.appendChild(renderer.domElement);
+
+// Generating geometry
+
+const geo = new THREE.BufferGeometry();
+
+let size = 100;
+let scale = 2.0;
+
+const simplex = new SimplexNoise();
+
+noiseFunc = function(x, y, hscale, fscale) {
+
+    let noise = 0.0;
+    const persistence = 0.5;
+    const lacunarity = 2.0;
+    const octaves = 8;
+    let pers = 0.5;
+    let lac = 2.25;
+    
+    let hmax = 1.0;
+
+    // noise = simplex.noise2D(x, y);
+    // noise += simplex.noise2D(2 * x, 2 * y) * 0.5;
+    // noise += simplex.noise2D(4 * x, 4 * y) * 0.25;
+    // noise += simplex.noise2D(8 * x, 8 * y) * 0.125;
+
+    for (let octave = 0; octave < octaves; octave++) {
+        hmax += pers;
+
+        noise += simplex.noise2D(x / lac * fscale, y / lac * fscale) * pers;
+        pers *= persistence;
+        lac /= lacunarity;
+    }
+
+    return [noise * hscale, hscale * hmax];
+}
+
+data = getProcGenData(size, scale, noiseFunc);
+verts = data[0];
+tris = data[1];
+colors = data[2];
+normals = data[3];
+
+geo.setIndex(tris);
+geo.setAttribute('position', new THREE.Float32BufferAttribute(verts, 3));
+geo.setAttribute('normal', new THREE.Float32BufferAttribute(normals, 3));
+geo.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+
+const mat = new THREE.MeshBasicMaterial( {
+    side: THREE.DoubleSide,
+    vertexColors: true
+});
+
+// Mesh - Scene Component
+const mesh = new THREE.Mesh(geo, mat);
+
+mesh.position.x = -scale * size;
+mesh.position.z = -scale * size;
+mesh.castShadow = true;
+mesh.receiveShadow = true;
+
+scene.add(mesh);
+
+// Light
+const light = new THREE.DirectionalLight(0xffffff, 0.5);
+light.position.x = 1;
+light.position.y = 0.5;
+light.position.z = 0.5;
+light.castShadow = true;
+scene.add(light);
+
+// Constants
+
+let mov = new THREE.Vector3(0.0, 0.0, 0.0);
+let mouseMove = false;
+let ogMouseX = 0;
+let ogMouseY = 0;
+let mouseX = 0.0;
+let mouseY = 0.0;
+const speed = 0.2;
+const sensitivity = 1.0 / Math.min(window.innerHeight, window.innerWidth);
+
+
+function anim() {
+    requestAnimationFrame(anim);
+
+    // camera.rotation.x += 0.1;
+    // mov.cross(camera.up);
+    // let v = new THREE.Vector3();
+    // camera.getWorldDirection(v);
+    
+    if (mouseMove) {
+        camera.rotation.y += (mouseX - ogMouseX) * sensitivity;
+        camera.rotation.x += (mouseY - ogMouseY) * sensitivity;
+    }
+    ogMouseX = mouseX;
+    ogMouseY = mouseY;
+
+    // light.position.x += 0.1;
+
+    let newx = mov.x * Math.cos(camera.rotation.y) + mov.z * Math.sin(camera.rotation.y);
+    let newz = -mov.x * Math.sin(camera.rotation.y) + mov.z * Math.cos(camera.rotation.y);
+    let dp = new THREE.Vector3(newx, mov.y, newz);
+    // let dp = forward.multiplyScalar(speed);
+    // dp = dp.multiply(mov);
+    camera.position.add(dp);
+
+    renderer.render(scene, camera);
+}
+
+function keyPress(event, key_down) {
+    if (event.defaultPrevented)
+        return;
+
+    const sc = key_down * 1.0;
+    
+    switch (event.key) {
+        case "ArrowDown":
+        case "s":
+            mov.z = sc;
+            break;
+        case "ArrowUp":
+        case "w":
+            mov.z = -sc;
+            break;
+        case "ArrowLeft":
+        case "a":
+            mov.x = -sc;
+            break;
+        case "ArrowRight":
+        case "d":
+            mov.x = sc;
+            break;
+        case " ":
+            mov.y = sc;
+            break;
+        case "Shift":
+            mov.y = -sc;
+            break;
+    }
+
+    event.preventDefault();
+}
+
+anim();
+window.addEventListener("keydown", function(event) {keyPress(event, true);}, true);
+window.addEventListener("keyup", function(event) {keyPress(event, false);}, true);
+
+window.addEventListener("mousedown", function(event) {
+    mouseMove = true;
+    ogMouseX = event.clientX;
+    ogMouseY = event.clientY;
+}, true);
+
+window.addEventListener("mousemove", function(event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+});
+
+window.addEventListener("mouseup", function(event) {
+    mouseMove = false;
+}, true);
---
+++ js/procgen.js
@@ -0,0 +1,107 @@
+function lerpColors(a, b, alpha) {
+    // (new THREE.Color()).mul
+    return a.add((b.sub(a)).multiplyScalar(alpha));
+}
+
+function colorFromPoint(z, zmax) {
+    const bot = new THREE.Color(0.0, 0.5, 0.0);
+    const top = new THREE.Color(0.8, 0.8, 0.8);
+    const v = lerpColors(bot, top, z / zmax);
+    
+    return v;
+}
+
+function vecFromArray(ind, array) {
+    return new THREE.Vector3(array[ind], array[ind + 1], array[ind + 2]);
+}
+
+/**
+ * @param verts the vertex positions
+ * @param normals the normals
+ * @param size the size of the map
+ * @param erosion_its the number of iterations of erosion
+ * @param kErosion How much erosion each iteration should do
+ * @param kDepsosition How much each iteration should deposit
+ * @param kFriction How much friction is applied to each iteration of erosion
+ * @return The modified vertex array
+ *
+**/
+function erode(verts, normals, size, erosion_its, kErosion, kDeposition, kFriction) {
+    const maxIterations = 100;
+    const speed = 1;
+    const iterationScale = 0.05; 
+
+    for (erosions = 0; erosions < erosion_its; ++erosions) {
+
+        let pos = new THREE.Vector2(Math.random() * size, Math.random() * size);
+        let lastPos = new THREE.Vector2(0.0, 0.0);
+        let vel = new THREE.Vector2(0.0, 0.0);
+        let sediment = 0;
+
+        for (i = 0; i < maxIterations; ++i) {
+            let index = Math.floor(pos.x) * size + Math.floor(pos.y);
+            let normal = vecFromArray(index, normals);
+
+            if (normal.y == 1.0)
+                break;
+
+            // the y value of the vertex
+            const deposit = sediment * kDeposition * normal.y;
+            const erosion = kErosion * (1 - normal.y) * Math.min(1, i * iterationScale);
+
+            verts[index + 1] += deposit - erosion;
+            sediment += erosion - deposit;
+
+            vel.x = kFriction * vel.x + normal.x * speed;
+            vel.z = kFriction * vel.z + normal.z * speed;
+            lastPos = pos;
+            pos += vel;
+        }
+    }
+    
+    return verts;
+}
+
+/**
+* @return An array with four elements, in the following order: vertex data (positions), 
+* index data (triangles), color data, and normals.
+*
+**/
+function getProcGenData(size, scale, func)
+{
+    verts = [];
+    tris = [];
+    colors = [];
+    normals = [];
+    // const hmax = scale;
+
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            x = i * scale;
+            z = j * scale;
+            [y, hmax] = func(x, z, scale * 20, 0.01);
+            verts.push(x, y, z);
+            const vin = i * size + j;
+    
+            if (i != size - 1 && j != size - 1) {
+    
+                tris.push(vin, vin + 1, vin + size + 1);
+                tris.push(vin + size + 1, vin + size, vin); // for some reason, it has to be in this order
+            }
+    
+            let p_v = new THREE.Vector3(verts[vin - 3], verts[vin - 2], verts[vin - 1]); // previous location
+            let tan = p_v.sub(new THREE.Vector3(x, y, z));
+            let norm = new THREE.Vector3(tan.x, -1.0, tan.z).normalize();
+
+            // normals.push(0.0, 1.0, 0.0);
+            normals.push(norm.x, norm.y, norm.z);
+            cc = colorFromPoint(y, hmax);
+            // (new THREE.Color()).r
+            colors.push(cc.r, cc.g, cc.b);
+        }
+    }
+
+    verts = erode(verts, normals, size * scale, 50000, 0.03, 0.05, 0.5);
+
+    return [verts, tris, colors, normals];
+}
